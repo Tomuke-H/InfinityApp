78c416eff9b971c887128c9a13db63ce70590ffe
Tom.Hohl <tom.hohl@PWs-iMac-4.local>
2022-08-10 13:56:28 -0600

    saving rolls v1

--- App.js
+++ App.js
@@ -4,16 +4,16 @@ import { StyleSheet, Text, View, Button } from 'react-native';
 import Calculator from './components/Calculator.js';
 import React, {useState} from 'react'
 
-const fakeProfile1 = {bs: 12, burst: 3, arm: 1}
-const fakeProfile2 = {bs: 13, burst: 1, arm: 1}
+const fakeProfile1 = {bs: 12, burst: 3, arm: 1, dam:13}
+const fakeProfile2 = {bs: 13, burst: 1, arm: 2, dam:14}
 
 export default function App() {
 
   return (
     <View style={styles.container}>
       <Text>Infinity Calculator App</Text>
-      <Text>Player 1: BS 12, Burst 3</Text>
-      <Text>Player 2: BS 13, Burst 1</Text>
+      <Text>Player 1: BS 12, Burst 3, Arm 1, Dam 13</Text>
+      <Text>Player 2: BS 13, Burst 1, Arm 2, Dam 14</Text>
       <Calculator player1={fakeProfile1} player2={fakeProfile2}/>
       <StatusBar style="auto" />
     </View>
--- components/Calculator.js
+++ components/Calculator.js
@@ -1,5 +1,5 @@
 import React, {useState } from 'react';
-import { Text, Button, View } from 'react-native';
+import { StyleSheet, Text, Button, View } from 'react-native';
 
 const Calculator = ({player1, player2}) => {
   const [results, setResults] = useState({rollResults:[]})
@@ -85,6 +85,39 @@ const Calculator = ({player1, player2}) => {
   }
 
 
+  // Damage calculating function
+  const damageFunction = (player1, player2, rollResults) => {
+    if(!rollResults.p1Unlocked && !rollResults.p2Unblocked){
+      return 'no damage'
+    }
+
+    // Roll saves
+    let max = Math.floor(21)
+    let min = Math.ceil(1)
+    let savingRolls = []
+    let unsavedDamage = 0
+    let unsavedHits = []
+
+    if(rollResults.p1Unblocked.length > 0){
+      for(let i = 0; i < rollResults.p1Unblocked.length; i++){
+        savingRolls.push(Math.floor(Math.random() * (max - min) + min));
+      }
+      unsavedHits = savingRolls.filter((r)=> {return r <= player1.dam - player2.arm})
+      unsavedDamage = unsavedHits.length
+    } else if(rollResults.p2Unblocked.length > 0) {
+      for(let i = 0; i < rollResults.p2Unblocked.length; i++){
+        savingRolls.push(Math.floor(Math.random() * (max - min) + min));
+      }
+      unsavedHits = savingRolls.filter((r)=> {return r <= player2.dam - player1.arm})
+      unsavedDamage = unsavedHits.length
+    } else {
+      return "error"
+    }
+
+    return {savingRolls, unsavedHits, unsavedDamage}
+  }
+
+
 
   // Master function
   const calcFunction = (player1, player2) => {
@@ -107,24 +140,48 @@ const Calculator = ({player1, player2}) => {
     // Compare the dice
     let rollResults = compareRolls(player1, player2, profile1Rolls, profile2Rolls)
 
+    // Calculate damage if applicable
+    let damageResults
+    if(rollResults.p1Unblocked && rollResults.p1Unblocked.length > 0 || rollResults.p2Unblocked && rollResults.p2Unblocked.length > 0){
+      damageResults = damageFunction(player1, player2, rollResults)
+      // damageResults = 'Could be damage!'
+    } else {
+      damageResults = 'No damage potential'
+    }
 
     // Set state for all the things
-    setResults({profile1Rolls, profile2Rolls, rollResults})
+    setResults({profile1Rolls, profile2Rolls, rollResults, damageResults})
   }
 
   
 
   // Display
   return (
-    <View>
+    <View style={styles.container}>
       <Button onPress={()=> calcFunction(player1, player2)} title="Run"/>
       <Text>Player 1's Roll:{JSON.stringify(results.profile1Rolls)}</Text>
       <Text>Player 2's Roll:{JSON.stringify(results.profile2Rolls)}</Text>
-      <Text>Player 1's Unblocked Roll:{results.rollResults.p1Unblocked ? JSON.stringify(results.rollResults.p1Unblocked) : "[]"}</Text>
-      <Text>Player 2's Unblocked Roll:{results.rollResults.p2Unblocked ? JSON.stringify(results.rollResults.p2Unblocked) : "[]"}</Text>
+      <Text></Text>
+      <Text>Player 1's Successes:{results.rollResults.p1Unblocked ? JSON.stringify(results.rollResults.p1Unblocked) : "[]"}</Text>
+      <Text>Player 2's Successes:{results.rollResults.p2Unblocked ? JSON.stringify(results.rollResults.p2Unblocked) : "[]"}</Text>
+      <Text></Text>
+      <Text>Saving Roll: {JSON.stringify(results.damageResults.savingRolls)}</Text>
+      <Text>Failed Saves: {JSON.stringify(results.damageResults.unsavedHits)}</Text>
+      <Text>Unsaved Damage: {JSON.stringify(results.damageResults.unsavedDamage)}</Text>
+      <Text> ------------------- </Text>
+      <Text></Text>
       <Text>Raw Data: {JSON.stringify(results)}</Text>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
 
 export default Calculator;
