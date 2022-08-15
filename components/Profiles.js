9efc7b962b238b630f538371cd3cfbf5ea4a1260
Tom.Hohl <tom.hohl@PWs-iMac-4.local>
2022-08-15 16:33:05 -0600

    dropdown example

--- components/Profiles.js
+++ components/Profiles.js
@@ -1,13 +1,38 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React,{ useState } from 'react'
+import { StyleSheet, View, Text, Button } from 'react-native'
+import { profileList } from './profileList'
+import DropDownPicker from 'react-native-dropdown-picker'
 
 const Profiles = ({active, reactive, setActive, setReactive}) => {
+  const [profiles, setProfiles] = useState([{label: 'Apple', value: 'apple'}, {label: 'Orange', value: 'orange'}])
+  const [openActive, setOpenActive] = useState(false)
+  const [activeValue, setActiveValue] = useState(null)
+  const [openReactive, setOpenReactive] = useState(false)
+
+  // const refineData = () => {
+  //   let refinedList = profileList.map((p)=> ({label: p.name, value: p.name}))
+  //   setProfiles(refinedList)
+  //   console.log(profileList)
+  //   console.log(profiles)
+  // }
+
+  // refineData()
   
   return (
     <View>
-      <Text></Text>
+      {/* <Button title="Refine" onPress={()=>refineData()} /> */}
+     
+      <DropDownPicker
+        open={openActive}
+        value={activeValue}
+        items={profiles}
+        setOpen={setOpenActive}
+        setValue={setActiveValue}
+        setItems={setProfiles}
+        containerStyle={{maxWidth:'70%'}}
+      />
       <Text>Active: </Text>
-      <Text>{active.name} BS{active.bs} ARM{active.arm}</Text>
+      <Text>{active.label} BS{active.bs} ARM{active.arm}</Text>
       <Text>Weapon: {active.weapon.name} Burst: {active.weapon.burst} Damage: {active.weapon.dam}</Text>
       <Text></Text>
       <Text>Reactive:</Text>
@@ -17,5 +42,14 @@ const Profiles = ({active, reactive, setActive, setReactive}) => {
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    maxWidth: '70%'
+  },
+});
 
 export default Profiles;
--- components/profileList.js
+++ components/profileList.js
@@ -1,4 +1,5 @@
 export const profileList = [
   {name: "Fusilier", bs: 12, weapon: {name: "Combi Rifle", burst: 3, dam: 13}, arm: 1, faction: "PanO"},
-  {name: "Thorakitai", bs: 12, weapon: {name: "Combi Rifle", burst: 1, dam: 13}, arm: 3, faction: "Aleph"}
+  {name: "Thorakitai", bs: 12, weapon: {name: "Combi Rifle", burst: 3, dam: 13}, arm: 3, faction: "Aleph"},
+  {name: "Morat Vanguard Infantry", bs: 11, weapon: {name: "Combi Rifle", burst: 3, dam: 13}, arm: 1, faction: "Combined Army"}
 ]
